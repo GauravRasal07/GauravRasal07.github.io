8f74fa25d9a3edc788d66c7d4664ed0419d17522
GauravRasal07 <69565906+GauravRasal07@users.noreply.github.com>
2021-07-05 23:44:31 +0530

    added images

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,31 +34,36 @@
   $(document).scroll(function() {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
+      $('#button').fadeIn();
     } else {
-      $('.scroll-to-top').fadeOut();
+      $('#button').fadeOut();
     }
   });
 
-})(jQuery); // End of use strict
+  var btn = $('#button');
 
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+  $(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+      btn.addClass('show');
+  } else {
+      btn.removeClass('show');
+  }
+  });
+
+  btn.on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({scrollTop:0}, '200');
+  });
+
+  function aos_init() {
+    AOS.init({
+        duration: 1000,
+        easing: "ease-in-out-back",
+        once: false
+    });
+  }
+  $(window).on('load', function () {
+    aos_init();
+  });
+
+})(jQuery);
