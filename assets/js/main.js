6eaf350d48474f3659b6bf03f451c1dfa2a62c88
GauravRasal07 <gauravvr.77@gmail.com>
2021-08-16 17:40:24 +0530

    disabled developer mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,6 +7,54 @@ window.addEventListener("scroll", (e) => {
   }
 })
 
+$('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop : $($(this).attr('href')).offset().top,
+    },500, 'linear')
+})
+
+document.onkeydown = function (e) {
+  if (e.keyCode == 123) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+    return false;
+  }
+  if (e.ctrlKey && e.keyCode == "U".charCodeAt(0)) {
+    return false;
+  }
+};
+
+function clickIE() {
+  if (document.all) {
+    alert(message);
+    return false;
+  }
+}
+function clickNS(e) {
+  if (document.layers || (document.getElementById && !document.all)) {
+    if (e.which == 2 || e.which == 3) {
+      return false;
+    }
+  }
+}
+if (document.layers) {
+  document.captureEvents(Event.MOUSEDOWN);
+  document.onmousedown = clickNS;
+} else {
+  document.onmouseup = clickNS;
+  document.oncontextmenu = clickIE;
+}
+
+document.oncontextmenu = new Function("return false");
+
 var addClassOnScroll = function () {
   var windowTop = $(window).scrollTop();
   $("section[id]").each(function (index, elem) {
